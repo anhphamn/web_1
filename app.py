7f006de75d57863be8245df59e228e3a9f80f1fd
anhphamn <meisama8706@gmail.com>
2026-08-18 22:45:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello</h1><p>Website Python đầu tiên</p>"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("index.html")
